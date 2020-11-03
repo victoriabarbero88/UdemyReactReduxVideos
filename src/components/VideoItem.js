22d1b4ed661feef7ed022cc0a7e7b431cb81ff60
Victoria <victoriabarbero88@gmail.com>
2020-11-03 15:37:44 +0100

    SearchBar, VideoList, VideoItem working and added some CSS

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import './VideoItem.css';
+import React from "react";
 
-const VideoItem = (props) => {
-  return <div>VideoItem</div>;
+const VideoItem = ({ video }) => {
+  return (
+    <div className="video-item item">
+      <img
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+        alt=""
+      />
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoItem;
